bd9a4b3d166cb335f493d4f61a9aaf2e91d0dfb4
unknown <mathieu.charpentier1993@gmail.com>
2023-11-23 13:11:48 +0100

    Update sidebar menu

--- Home.js
+++ Home.js
@@ -2,16 +2,16 @@ let civilisationMenu = document.getElementById("civilisation");
 let characterMenu = document.getElementById("character");
 let constructionMenu = document.getElementById("construction");
 
+civilisationMenu.style.display = "none";
+characterMenu.style.display = "none";
+constructionMenu.style.display = "none";
+
 /**
  * Make appear one sidebar Menu div and disappear the other  
  * @param {String} id id of the div that will appear or disappear
  */
 function showOrHideOnClick(id)
 {
-    civilisationMenu.style.display = "none";
-    characterMenu.style.display = "none";
-    constructionMenu.style.display = "none";
-
     if(id == "civilisation")
     {
         if(civilisationMenu.style.display == "none")
